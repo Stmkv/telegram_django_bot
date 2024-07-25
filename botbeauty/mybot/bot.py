a7fd04ec3cd04cee40f1f4acd4134a2d52319b45
Stmkv <stepkamakarov1999@mail.ru>
2024-07-25 12:48:25 +0300

    Добавил функцию оплаты

--- botbeauty/mybot/bot.py
+++ botbeauty/mybot/bot.py
@@ -1,7 +1,10 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, LabeledPrice
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from dotenv import load_dotenv
+
+
+
 
 
 def start(update: Update, context: CallbackContext):
@@ -15,7 +18,31 @@ def start(update: Update, context: CallbackContext):
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Выберите опцию:', 
                               reply_markup=reply_markup)
-    
+
+
+def pay(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    title = "Тестовая оплата"
+    description = "Оплата услуг салона Beauty-City"
+    payload = "Стрижка машинкой"
+    currency = "RUB"
+    prices = [LabeledPrice("Тестовая оплата", 10000)]
+
+    provide_token = context.bot_data['provide_token']# Цена в копейках
+
+    context.bot.send_invoice(
+        chat_id=chat_id,
+        title=title,
+        description=description,
+        payload=payload,
+        provider_token=provide_token,
+        currency=currency,
+        prices=prices,
+        start_parameter="test-payment",
+        need_name=True,
+        need_phone_number=True,
+    )
+
 
 def button(update: Update, context: CallbackContext):
     query = update.callback_query
@@ -25,10 +52,15 @@ def button(update: Update, context: CallbackContext):
 
 def main():
     load_dotenv()
-    token = os.environ['BOT_TOKEN']
-    updater = Updater(token, use_context=True)
+    provide_token = os.environ['PAYMENT_PROVIDER_TOKEN']
+    telegram_token = os.environ['BOT_TOKEN']
+    updater = Updater(telegram_token, use_context=True)
+
+    updater.dispatcher.bot_data['provide_token'] = provide_token
     updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('pay', pay))
     updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
     updater.start_polling()
     updater.idle()
 
